6cd73f48efa108fa3afa84565fb601f9017df1d0
MradulGandhi <gandhisir80@gmail.com>
2025-09-05 00:22:18 +0530

    App listen

--- src/index.ts
+++ src/index.ts
@@ -34,11 +34,13 @@ app.use('/api/therapies', therapiesRouter);
 mongoose.connect(process.env.MONGO_URI || '', {})
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+   
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
     process.exit(1);
   });
+
+   app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
